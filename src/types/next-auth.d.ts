6966ac9825c82668b1d7ce534c79dec9d79efbf8
davidegiudice <davidegiudice@icloud.com>
2025-01-13 12:50:03 +0100

    separate auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,18 +1,20 @@
-import 'next-auth'
+import { DefaultSession } from 'next-auth'
 import { Role } from '@prisma/client'
 
 declare module 'next-auth' {
-  interface User {
-    role?: Role
+  interface Session {
+    user: {
+      role: Role
+    } & DefaultSession['user']
   }
 
-  interface Session {
-    user?: User
+  interface User {
+    role: Role
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    role?: Role
+    role: Role
   }
 } 
