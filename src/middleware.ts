b9dae2db911ee896147959abd4c8057576a3fc2c
davidegiudice <davidegiudice@icloud.com>
2025-01-14 12:29:51 +0100

    handled auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,11 @@
 import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
-import type { Role } from '@prisma/client'
-import type { WithAuthArgs } from 'next-auth/middleware'
+import type { NextAuthMiddlewareOptions } from 'next-auth/middleware'
 
 export default withAuth(
-  function middleware(req: WithAuthArgs) {
+  async function middleware(req) {
     const token = req.nextauth?.token
-    
+
     if (token?.role !== 'ADMIN' && token?.role !== 'EDITOR') {
       return NextResponse.redirect(new URL('/login', req.url))
     }
