91d750b3107eff2dbc6f15f496c78ac5ab14f09f
davidegiudice <davidegiudice@icloud.com>
2025-01-14 12:37:42 +0100

    handled auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
+import type { NextRequest } from 'next/server'
 
 export default withAuth(
-  async function middleware(req) {
+  async function middleware(
+    req: NextRequest & { nextauth: { token: { role?: string } } }
+  ) {
     const token = req.nextauth?.token
 
     if (token?.role !== 'ADMIN' && token?.role !== 'EDITOR') {
